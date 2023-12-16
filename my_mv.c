8ea2f33af7d4d02973e8af3b14eb2cabc61ff20a
laiaespluga <146718932+laiaespluga@users.noreply.github.com>
2023-12-16 11:52:20 +0100

    Update my_mv.c

--- my_mv.c
+++ my_mv.c
@@ -1,37 +1,41 @@
-#include <stdio.h>
+#include <stdio.h> //Afegim les biblioteques corresponents que utilitzarem durant el programa
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 
-int my_mv() {
-    char nom_arxiu[250], nou_arxiu[250];
-    char linia[250];
+int main() { //Definim la funció principal
+  char buffer[406]; //Declarem un array de tipus 'buffer' per emmagatzemar dades temporals
+  char nom_arxiu[250], nou_arxiu[250]; //Declarem dos arrays de tipus 'char' per emmagatzemar els noms de l'arxiu original i el nou arxiu
+  ssize_t bytesLlegits; //Variable que emmagatzema el número de bytes llegits d'un fitxer
 
-    printf("Introdueix el nom de l'arxiu: ");
+    printf("Introdueix el nom de l'arxiu: "); //Demanem a l'usuari que introdueixi el nom de l'arxiu
     scanf("%s", nom_arxiu);
-    printf("Introdueix el nou nom/lloc de l'arxiu: ");
+    printf("Introdueix el nou nom/lloc de l'arxiu: "); //Demanem a l'usuari que introdueixi el nou nom o el lloc de l'arxiu
     scanf("%s", nou_arxiu);
 
-    int orig = open(nom_arxiu, O_RDONLY);
-    int desc = open(nou_arxiu, O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
-
-    if (orig < 0) {
-        perror("Error al obrir l'arxiu (l'arxiu no existeix)");
-        return 1;
+   int desc = open(nou_arxiu, O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU); //Obrim l'arxiu amb permisos de lectura, escriptura i execució per l'usuari
+   if (desc == -1) { //Si hi ha hagut un error al obrir l'arxiu, mostra un missatge d'error i acaba el programa
+        perror("Error al crear el nou arxiu.");
+        return;
+      }
+    int descArxiuOriginal = open(nom_arxiu, O_RDONLY); //Obrim l'arxiu original en mode lectura
+    if (descArxiuOriginal == -1) { //Si hi ha hagut un error al obrir l'arxiu, mostra un missatge d'error, tanca l'arxiu i acaba el programa
+      perror("Error a l'obrir l'arxiu original");
+      close(desc);
+          return;
+      }
+  
+    while ((bytesLlegits = read(descArxiuOriginal, buffer, sizeof(buffer))) > 0) { //Utilitzem el bucle while per llegir les dades del fitxer original i escriure-les al nou arxiu
+      write(desc, buffer, bytesLlegits);
     }
-    if (desc < 0) {
-        perror("Error al crear el nou arxiu");
-        return 1;
-    } else {
-        while (read(linia, sizeof(linia), orig) != NULL) {
-            write(desc, linia, strlen(linia));
-        }
-    }
 
-    close(orig);
-    close(desc);
-    unlink(nom_arxiu);
+  close(descArxiuOriginal); //Després de completar la còpia tanquem els dos arxius
+  close(desc);
 
+  if (unlink(nom_arxiu) == -1) { //Utilitzem la comanda 'unlink' per eliminar l'arxiu original
+      perror("Error al borrar l'arxiu original"); //Si hi ha un error es mostra per pantalla un missatge d'error
+  }
 
-    return 0;
+  printf("L'arxiu ha estat mogut."); //Si no hi ha hagut cap error, es mostra per pantalla que el programa s'ha completat correctament
+
 }
